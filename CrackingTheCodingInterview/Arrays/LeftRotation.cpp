09a2947ce9516c019b76e8b0ce2430251bbc4792
Abhishek Agrawal <cruxrebels@gmail.com>
2016-10-14 23:34:38 +0530

    Update LeftRotation.cpp
    Added the code.

--- CrackingTheCodingInterview/Arrays/LeftRotation.cpp
+++ CrackingTheCodingInterview/Arrays/LeftRotation.cpp
@@ -32,3 +32,53 @@ Thus, we print the array's final state as a single line of space-separated value
 
 Problem: https://www.hackerrank.com/challenges/ctci-array-left-rotation
 */
+
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> array_left_rotation(vector<int> a, int n, int k) {
+    vector<int> result;
+    for (auto i = k; i<n; ++i)
+        result.emplace_back(a[i]);
+    for (auto j = 0; j<k; ++j)
+        result.emplace_back(a[j]);
+    return result;
+}
+
+int main(){
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int a_i = 0;a_i < n;a_i++){
+        cin >> a[a_i];
+    }
+    vector<int> output = array_left_rotation(a, n, k);
+    for(int i = 0; i < n;i++)
+        cout << output[i] << " ";
+    cout << endl;
+    return 0;
+}
+
