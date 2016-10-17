f04c2d2a665d55c4f049bb79bc42da703a59bc9a
Abhishek Agrawal <cruxrebels@gmail.com>
2016-10-17 23:55:23 +0530

    Create DivisiblePairsSum.cpp
    Added the problem statement.

---
+++ LinkedInPlacements/DivisiblePairsSum.cpp
@@ -0,0 +1,27 @@
+/*
+You are given an array of  integers, , and a positive integer, . Find and print the number of pairs where  and  +  is evenly divisible by .
+
+Input Format
+
+The first line contains  space-separated integers,  and , respectively. 
+The second line contains  space-separated integers describing the respective values of .
+
+Constraints
+
+Output Format
+
+Print the number of  pairs where  and  +  is evenly divisible by .
+
+Sample Input
+
+6 3
+1 3 2 6 1 2
+Sample Output
+
+ 5
+Explanation
+
+Here are the  valid pairs:
+
+https://www.hackerrank.com/challenges/linkedin-practice-divisible-sum-pairs
+*/
