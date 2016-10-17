68eecc5dc052a2f77dd40e92b2ca175f22dd5060
Abhishek Agrawal <cruxrebels@gmail.com>
2016-10-17 23:55:57 +0530

    Update DivisiblePairsSum.cpp
    Added the code.

--- LinkedInPlacements/DivisiblePairsSum.cpp
+++ LinkedInPlacements/DivisiblePairsSum.cpp
@@ -25,3 +25,36 @@ Here are the  valid pairs:
 
 https://www.hackerrank.com/challenges/linkedin-practice-divisible-sum-pairs
 */
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int pairs(vector<int>& input, int k) {
+    auto size = input.size();
+    int res = 0;
+    for (auto i = 0; i<size-1; ++i) {
+        for (auto j = i+1; j<size; ++j) {
+            if ((input[i]+input[j])%k == 0)
+                ++res;
+        }
+    }
+    return res;
+}
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int n = 0, k = 0;
+    cin >> n >> k;
+    vector<int> input;
+    while(n--) {
+        int i = 0;
+        cin >> i;
+        input.emplace_back(i);
+    }
+    cout << pairs(input, k);   
+    return 0;
+}
