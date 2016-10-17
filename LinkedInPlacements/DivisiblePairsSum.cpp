07ec8d8d0b963a10bf482c71422dd1c7f741ed45
Abhishek Agrawal <cruxrebels@gmail.com>
2016-10-18 00:07:02 +0530

    Update DivisiblePairsSum.cpp
    Added O(N) linear time solution.

--- LinkedInPlacements/DivisiblePairsSum.cpp
+++ LinkedInPlacements/DivisiblePairsSum.cpp
@@ -33,7 +33,55 @@ https://www.hackerrank.com/challenges/linkedin-practice-divisible-sum-pairs
 #include <algorithm>
 using namespace std;
 
-int pairs(vector<int>& input, int k) {
+// O(N) solution
+/*
+The idea is that you separate elements into buckets depending on their mod k. For example, you have the elements: 1 3 2 6 4 5 9 and 
+k = 3
+mod 3 == 0 : 3 6 9
+mod 3 == 1 : 1 4
+mod 3 == 2 : 2 5
+Now, you can make pairs like so: Elements with mod 3 == 0 will match with elements with (3 - 0) mod k = 0, so other elements in 
+the mod 3 == 0 list, like so:
+(3, 6) (3, 9) (6, 9)
+There will be n * (n - 1) / 2 such pairs, where n is length of the list, because the list is the same and i != j. 
+Elements with mod 3 == 1 will match with elements with (3 - 1) mod k = 2, so elements in the mod 3 == 2 list, like so:
+(1, 2) (1, 5) (4, 2) (4, 5)
+There will be n * k such elements, where n is the length of the first list, and k of the second.
+Because you don't need to print the actual pairs, you need to store only the number of elements in each list.
+*/
+int pairs(vector<int>& input, vector<int>& bucket, int k) {
+    auto size = input.size();    
+    int res = 0;       
+    
+    res += (bucket[0]*(bucket[0]-1))/2;
+    
+    for (auto i = 1; i<=k/2 && i != k-i; ++i)
+        res += bucket[i]*bucket[k-i];
+    
+    if (k%2 == 0)
+        res += (bucket[k/2]*(bucket[k/2]-1))/2;
+    return res;
+}
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int n = 0, k = 0;
+    cin >> n >> k;
+    vector<int> input;
+    vector<int> bucket(k, 0);
+    while(n--) {
+        int i = 0;
+        cin >> i;
+        ++bucket[i%k];
+        input.emplace_back(i);
+    }
+    cout << pairs(input, bucket, k);   
+    return 0;
+}
+
+
+// O(N^2) solution
+/*int pairs(vector<int>& input, int k) {     
     auto size = input.size();
     int res = 0;
     for (auto i = 0; i<size-1; ++i) {
@@ -57,4 +105,4 @@ int main() {
     }
     cout << pairs(input, k);   
     return 0;
-}
+}*/
